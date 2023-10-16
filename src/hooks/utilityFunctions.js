f537704d066a5f89101b05f9bdb40951340aa63b
JustinRivera-9 <justin_rivera9@outlook.com>
2023-10-15 21:56:41 -0600

    results now mapped correctly. Just need to fix styles but logic is complete

--- src/hooks/utilityFunctions.js
+++ src/hooks/utilityFunctions.js
@@ -1,3 +1,7 @@
 export function answerStyles(userAnswer, correctAnswer) {}
 
-export function gradeQuiz(quizInfo) {}
+export function gradeQuiz(quizInfo) {
+  return quizInfo.filter(
+    (el) => el.userAnswer === el.questions[el.currentQuestion].correctAnswer
+  ).length;
+}
--- src/quiz-page/Reuslts.js
+++ src/quiz-page/Reuslts.js
@@ -1,13 +1,27 @@
+// import { useState } from "react";
+import { gradeQuiz } from "../hooks/utilityFunctions";
+
 function Reuslts({ quizResults }) {
-  console.log(quizResults);
+  //   const [numCorrect, setNumCorrect] = useState(() => gradeQuiz(quizResults));
+  const numCorrect = gradeQuiz(quizResults);
 
   return (
     <div className="flex justify-center flex-col">
       <span className="text-3xl text-white">
-        You got 4 out of 10 correct!{" "}
-        <strong style={{ color: "#2196f3" }}>(40%)</strong>
+        {`You got ${numCorrect} out of 10 correct!`}
+        <strong style={{ color: "#2196f3" }}>{`(${
+          (numCorrect / 10) * 100
+        }%)`}</strong>
       </span>
-      <div className="text-3xl text-white">QUESTION SECTION</div>
+      {quizResults.map((el, i) => (
+        <div key={i} className="flex justify-center flex-col">
+          <div className="text-xl text-white py-4">{`Question #${i + 1}: ${
+            el.questions[i].question
+          }`}</div>
+          <div className="text-xl text-white py-2">{`The correct answer is: ${el.questions[i].correctAnswer}`}</div>
+          <div className="text-xl text-white py-2">{`You chose: ${el.userAnswer}`}</div>
+        </div>
+      ))}
     </div>
   );
 }
