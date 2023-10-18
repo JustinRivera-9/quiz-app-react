05ef4d9e795af11f6761e66eb9cb0e0e56027258
JustinRivera-9 <justin_rivera9@outlook.com>
2023-10-17 23:01:07 -0600

    fixed text fields in progress bar

--- src/quiz-page/SectionProgress.js
+++ src/quiz-page/SectionProgress.js
@@ -1,8 +1,10 @@
+import { formatString } from "../hooks/utilityFunctions.js";
+
 function SectionProgress({ curQuestion, category, difficulty }) {
   return (
     <>
       <div className="flex justify-around">
-        <div className="text-xl text-gray-200">{difficulty}</div>
+        <div className="text-xl text-gray-200">{formatString(difficulty)}</div>
         <div className="text-2xl font-bold" style={{ color: "#2196f3" }}>
           {(curQuestion / 10) * 100}%
         </div>
